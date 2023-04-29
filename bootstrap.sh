e0efdfbfeec4dee57d1c00836711bb244c0639aa
cding <cding@us.ibm.com>
2023-04-29 19:21:14 -0400

    initial checkin

---
+++ bootstrap.sh
@@ -0,0 +1,7 @@
+echo "# wisdom" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/csawisc/wisdom.git
+git push -u origin main
---
+++ checkdns.sh
@@ -0,0 +1,1 @@
+dig wisdom.comkraft.COM +nostats +nocomments +nocmd
